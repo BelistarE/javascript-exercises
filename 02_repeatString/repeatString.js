8253d96fda6c1193a8400e2c1b3f7e73ad706d44
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-06-14 12:00:08 -0700

    completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    let result = "";
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    return result;
 };
 
+repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
