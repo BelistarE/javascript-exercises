415bd17396def927bd4affdc0f53877545ab1f93
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-06-14 21:23:03 -0700

    added test file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const characters = str.split('');
+
+    const reverseCharacters = [];
+    for (let i = characters.length - 1; i >= 0; i--) {
+        // Push each character to the reversedCharacters array
+        reverseCharacters.push(characters[i]);
+    }
+
+    const reversedString = reverseCharacters.join('');
+    
+    // Return the reversed string
+    return reverseString;
 
 };
-
+//testing here
+reverseString("ian james halliday");
 // Do not edit below this line
 module.exports = reverseString;
